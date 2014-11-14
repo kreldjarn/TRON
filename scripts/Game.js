3d4cac311eb153870699bbfcea06332d337789de
Kristján Eldjárn <kristjan@eldjarn.net>
2014-11-14 16:07:07 +0000

    Wtf

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,8 +1,16 @@
 function Game(descr){
-	for (var property in descr) {
-	this[property] = descr[property];
+		for (var property in descr) {
+		this[property] = descr[property];
+	}
 }
-}
+
+Game.protoype.reset = function()
+{
+	this.score=0;
+	this.state=0;
+	this.numbOfEnemies=1;
+	this.gridChoice="A";
+};
 
 var g_game = new Game({
     score: 0,
@@ -12,10 +20,3 @@ var g_game = new Game({
 });
 
 
-Game.protoype.reset = function()
-{
-	this.score=0;
-	this.state=0;
-	this.numbOfEnemies=1;
-	this.gridChoice="A";
-}
